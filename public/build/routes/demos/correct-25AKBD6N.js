6059d635b72cdf6665d379b2eaf5352e2b423759
dbp <byronpantoja@gmail.com>
2021-12-07 08:55:11 +0800

    adding modules

---
+++ public/build/routes/demos/correct-25AKBD6N.js
@@ -0,0 +1,17 @@
+import {
+  React,
+  init_react
+} from "/build/_shared/chunk-E7VMOUYL.js";
+
+// browser-route-module:/Users/dbp/Developer/Plan-B-Projects/833remix-2/app/routes/demos/correct.tsx?browser
+init_react();
+
+// app/routes/demos/correct.tsx
+init_react();
+function NiceWork() {
+  return /* @__PURE__ */ React.createElement("h1", null, "You got it right!");
+}
+export {
+  NiceWork as default
+};
+//# sourceMappingURL=/build/routes/demos/correct-25AKBD6N.js.map
